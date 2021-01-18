a9d3efbe5ae383017387b91e103c083756a2fb7d
ookomanuel <ookomanuel@gmail.com>
2021-01-18 12:51:29 +0300

    Added future & past verbs plus a Noun & Adj5

--- madlibs.py
+++ madlibs.py
@@ -19,6 +19,10 @@ adj4 = input("Adjective: ")
 adverb = input("Adverb for burning: ")
 verb4 = input("2nd Last Verb: ")
 last_verb = input("Last Verb: ")
+noun1 = input("Noun: ")
+adj5 = input("Adjective: ")
+
+
 
 #madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
 #I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
@@ -29,6 +33,7 @@ geschichte = f"I once met a {adj} man. He was {adj1} and {adj2} about Life.\
     He had a flair about him because all he went near seemed to turn a little {precious_mineral}.\
         He could {verb1}, {verb2} and {verb3} the piano.\
             Yes, he was {adj3} with a {adj4} personality and inside he burned {adverb}!\
-                This young man always {verb4} about his future while trying to {last_verb} his past."
+                This young man always {verb4} about his future while trying to {last_verb} his past.\
+                    This {noun1} wasn't always {adj5}!"
 
 print(geschichte)
